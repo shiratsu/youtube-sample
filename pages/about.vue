d0af2cb24cf6325ee2cd50e92751f493af35fbe5
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-06-06 08:32:15 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>About @shiratsu</v-card-title>
+        </v-card>
+        <nuxt-link to="/">index</nuxt-link>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
